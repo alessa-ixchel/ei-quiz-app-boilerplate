8761d64017dd002da503874605e47a4042eb68af
Alessa Walle <alessawalle@gmail.com>
2020-07-08 16:35:20 -0400

    update js and index

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Example store structure
  */
@@ -56,3 +58,14 @@ const store = {
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+
+
+//USER REQUIREMENTS
+//one at a time
+//Display which question number user is on
+//Display number correct with jQuery
+//If answer is correct, move forward, otherwise ask to correct with if/else statementS
+//Show results at end
+
+//TECHNICAL REQUIREMENTS
+//Create a function
